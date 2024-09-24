1adaebe5defa3e326635c58de36239481efed252
Ariari04 <anime.beksultan@gmail.com>
2024-09-24 17:27:49 +0600

    lab2 done

--- asd.py
+++ asd.py
@@ -1,2 +1,11 @@
-import random
-print(random.randint(1,5))
+a = input()
+b = ""
+c = ""
+for i in range(len(a)):
+    if a[i] == '=':
+        b = a[0: i]
+        c = a[i+1: len(a)]
+if eval(b) == eval(c):
+    print("YES")
+else:
+    print("NO")
